996fac6c72850aacff834e7639b12fc968a3752c
Corran <corranharker@hotmail.co.uk>
2015-05-20 18:49:15 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,11 @@
-exports.names = ['.help', '.support'];
+exports.names = ['!help', '!support'];
 exports.hidden = false;
 exports.enabled = true;
 exports.matchStart = true;
 exports.handler = function (data) {
 
     if (config.slack.webhookUrl !== '') {
-        bot.sendChat('Need help? Mention @staff. No mods around? Try .callmod or contact a Brand Ambassador in http://plug.dj/support');
+        bot.sendChat('Need help? Mention @staff. No mods around? Try !callmod or contact a Brand Ambassador in http://plug.dj/support');
     }
     else {
         bot.sendChat('Need help? Mention @staff. No mods around? Contact a Brand Ambassador in http://plug.dj/support');
